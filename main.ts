bbf78a6ba190f39777c64b7b2f0c7330af121fda
Fran Diaz <100407685+ProfeFDV@users.noreply.github.com>
2022-02-28 16:50:02 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,12 +5,16 @@ input.onButtonPressed(Button.A, function () {
         pins.digitalWritePin(DigitalPin.P2, 1)
         basic.pause(500)
     }
+    pins.digitalWritePin(DigitalPin.P2, 0)
 })
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < 4; index++) {
         pins.digitalWritePin(DigitalPin.P2, 0)
-        basic.pause(500)
+        basic.pause(1000)
         pins.digitalWritePin(DigitalPin.P2, 1)
-        basic.pause(500)
+        basic.pause(1000)
     }
+    pins.digitalWritePin(DigitalPin.P2, 0)
 })
+led.enable(false)
+pins.digitalWritePin(DigitalPin.P2, 0)
